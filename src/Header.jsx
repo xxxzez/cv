518f1485eafcf2848da96d0c1c9c0b6c6a48adeb
Matvei Bohush <bohush.mathew@gmail.com>
2020-04-02 14:11:40 +0300

    add basic structure

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.header}>
+       
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Main.module.css";
+
+function Main() {
+  return (
+    <div className={styles.main}>
+       
+    </div>
+  );
+}
+
+export default Main;
